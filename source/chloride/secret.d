9b80f7612b13c024422bfb00b7c53498c1940424
Thayne McCombs <astrothayne@gmail.com>
2016-03-30 23:23:00 -0600

    Add secret key encryption

---
+++ source/chloride/secret.d
@@ -0,0 +1,138 @@
+module chloride.secret;
+
+import chloride.core;
+import chloride.random : fillRandom;
+
+import std.array;
+
+import sodium.crypto_secretbox;
+
+alias SecretKeyLength = crypto_secretbox_KEYBYTES;
+alias SecretNonceLength = crypto_secretbox_NONCEBYTES;
+alias SecretMacLength = crypto_secretbox_MACBYTES;
+
+/**
+ * A struct containing both the encrypted cipher (using secret key encryption)
+ * and the nonce used to encrypt it.
+ */
+struct SecretBox {
+    ubyte[] cipher;
+    ubyte[SecretNonceLength] nonce;
+}
+
+/**
+ * Wrapper around `crypto_secretbox_easy`. Encrypts a message using a nonce and key
+ * and returns the cipher with MAC as a prefix.
+ */
+ubyte[] encryptSecretBox(in ubyte[] message,
+                         in ubyte[SecretNonceLength] nonce,
+                         in ubyte[SecretKeyLength] key) {
+    auto cipher = uninitializedArray!(ubyte[])(message.length + SecretMacLength);
+    auto result = crypto_secretbox_easy(cipher.ptr, message.ptr, message.length,
+                          nonce.ptr, key.ptr);
+    enforceSodium(result == 0);
+    return cipher;
+}
+
+
+/**
+ * Wrapper around `crypto_secretbox_open_easy`. Decrypts a cipher using a nonce and key
+ * and returns the decrypted message.
+ * If decryption fails, null is returned.
+ */
+ubyte[] decryptSecretBox(in ubyte[] cipher,
+                         in ubyte[SecretNonceLength] nonce,
+                         in ubyte[SecretKeyLength] key) in {
+    assert(cipher.length > SecretMacLength);
+} body {
+    auto message = uninitializedArray!(ubyte[])(cipher.length - SecretMacLength);
+    auto result = crypto_secretbox_open_easy(message.ptr, cipher.ptr, cipher.length,
+                                             nonce.ptr, key.ptr);
+    if (result == 0) {
+        return message;
+    } else {
+        return null;
+    }
+}
+/**
+ * Wrapper around `crypto_secretbox_easy` that encrypts in place.
+ * `buffer` is changed in place from the message to the cipher. Note
+ * that it will be reallocated to be large enough to include the MAC, which
+ * may cause the array to be copied.
+ */
+void encryptSecretBoxInPlace(ref ubyte[] buffer,
+                             in ubyte[SecretNonceLength] nonce,
+                             in ubyte[SecretKeyLength] key) {
+    auto messageLen = buffer.length;
+    buffer.length += SecretMacLength; // make sure we have enough space for the MAC
+    auto result = crypto_secretbox_easy(buffer.ptr, buffer.ptr, messageLen, nonce.ptr, key.ptr);
+    enforceSodium(result == 0);
+}
+
+/**
+ * Wrapper around `crypto_secretbox_open_easy` that decrypts in place.
+ * `buffer` is changed in place from the cipher to the decrypted message.
+ * If decryption is successful, true is returned and `buffer` is updated with
+ * the length of the decrypted message, which will be shorter than the cipher.
+ * Otherwise true will be returned.
+ */
+bool decryptSecretBoxInPlace(ref ubyte[] buffer,
+                             in ubyte[SecretNonceLength] nonce,
+                             in ubyte[SecretKeyLength] key) in {
+    assert(buffer.length > SecretMacLength);
+} body {
+    auto result = crypto_secretbox_open_easy(buffer.ptr, buffer.ptr, buffer.length,
+                                             nonce.ptr, key.ptr);
+    if (result == 0) {
+        buffer = buffer[0 .. $ - SecretMacLength];
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Encrypt `message` using `key` and a newly generated nonce. Return a `SecreBox` with
+ * the encrypted ciphertext and the generated nonce.
+ */
+SecretBox encrypt(in ubyte[] message, in ubyte[SecretKeyLength] key) {
+    SecretBox result;
+    result.nonce = makeNonce();
+    result.cipher = encryptSecretBox(message, result.nonce, key);
+    return result;
+}
+
+/**
+ * Decrypt `box` which contains the ciphertext and the nonce used to generate it.
+ * If decryption fails it returns null.
+ */
+ubyte[] decrypt(in SecretBox box, in ubyte[SecretKeyLength] key) {
+    return decryptSecretBox(box.cipher, box.nonce, key);
+}
+
+/**
+ * Generate a nonce suitable for secret key encryption.
+ */
+ubyte[SecretNonceLength] makeNonce() {
+    ubyte[SecretNonceLength] nonce = void;
+    fillRandom(nonce);
+    return nonce;
+}
+
+/**
+ * Generate a key suitable for secret key encryption.
+ */
+ubyte[SecretKeyLength] makeSecretKey() {
+    ubyte[SecretKeyLength] key = void;
+    fillRandom(key);
+    return key;
+}
+
+///
+unittest {
+    import std.string : representation;
+    immutable ubyte[] message = representation("hello");
+    immutable key = makeSecretKey();
+    auto box = encrypt(message, key);
+    assert(decrypt(box, key) == message);
+}
